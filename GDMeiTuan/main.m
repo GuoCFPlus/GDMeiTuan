3d5f2918e9d02e05f1eb943f1128657fb017733c
郭彩风 <guocf@cbayel.com>
2017-02-17 09:43:58 +0800

    Initial Commit

---
+++ GDMeiTuan/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GDMeiTuan
+//
+//  Created by 郭彩风 on 2017/2/17.
+//  Copyright © 2017年 cbayel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
